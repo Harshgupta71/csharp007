c3ed5ae8daa5fdab36ab69599dd1f3b0de6896f2
Harsh Gupta <Harshdeepgupta49@gmail.com>
2019-11-04 05:04:02 -0500

    added

---
+++ csharp007/NameValue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace csharp007
+{
+    /// <summary>
+    /// It can be used to store key/value pairs in terms on Name/Value
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class NameValue<T>
+    {
+        /// <summary>
+        /// Name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Value
+        /// </summary>
+        public T Value { get; set; }
+
+        public NameValue()
+        {
+
+        }
+
+        public NameValue(string name, T value)
+        {
+            Name = name;
+            Value = value;
+        }
+    }
+}
